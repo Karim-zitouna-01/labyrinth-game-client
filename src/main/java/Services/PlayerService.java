7aee78d33c17077bf132798b5cc439cb149ba235
Karim-zitouna-01 <karimzitouna01@gmail.com>
2025-01-21 23:15:26 +0100

    added service classes (no implementation)

---
+++ src/main/java/Services/PlayerService.java
@@ -0,0 +1,15 @@
+package Services;
+
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import Models.*;
+
+import java.net.http.*;
+import java.net.URI;
+import java.net.http.HttpResponse;
+import java.util.Optional;
+
+public class PlayerService {
+
+}
